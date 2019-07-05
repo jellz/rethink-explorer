8aa4a0769c95ff8df0957cec8853ce4ae748cc19
Daniel Gulic <danielgulic@gmail.com>
2019-07-05 22:32:27 +1000

    list databases

--- src/server.js
+++ src/server.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const logger = require('morgan');
+const rethinkdb = require('rethinkdbdash');
 
+const r = rethinkdb();
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
 app.use(logger('dev'));
+app.use('/assets', express.static('src/static'))
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'test' });
+app.get('/', async (req, res) => {
+  const databases = [];
+  const dbNames = await r.dbList();
+  for (let i = 0; i < dbNames.length; i++) {
+    console.log('test1');
+    const dbName = dbNames[i];
+    console.log('name', dbName);
+    const dbTables = await r.db(dbName).tableList();
+    console.log('test2');
+    databases.push({
+      name: dbName,
+      tables: dbTables
+    })
+  }
+  console.log('test3');
+  res.render('index', {
+    databases
+  });
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`rethink-explorer listening on port ${port}`));
+app.listen(port, () =>
+  console.log(`rethink-explorer listening on port ${port}`)
+);
